f66a8cb9925b9b7ec33b348c9a6d65358152b5a9
Mathias Baumgartinger <hiasbaum@gmail.com>
2025-07-17 09:46:46 +0200

    refactor(cleanup): remove unused files

--- static/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
-
-console.log('Vue app mounted successfully!') // Log to confirm the app has mounted
